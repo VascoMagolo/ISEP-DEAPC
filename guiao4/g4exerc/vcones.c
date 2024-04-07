ecc527adf65dc947fa1dbb3bba1c2fea2025b6f8
Vasco Magolo <vascomagolo@gmail.com>
2024-04-07 21:57:31 +0100

    Finished g4exerc, started g42exerc

--- guiao4/g4exerc/vcones.c
+++ guiao4/g4exerc/vcones.c
@@ -14,14 +14,15 @@ int main(int argc, char *argv[]){
     cone *v=(cone *)calloc(atoi(argv[1]),sizeof(cone));
     
    for (int i = 0; i < atoi(argv[1]); i++)
-{
+    {
     new_cone(v+i);
-}
+    }
 
     printf("Cones created: %d\n", atoi(argv[1]));
     for (int i = 0; i < atoi(argv[1]); i++)
     {
         printf("Cone %d: radius %.2f, height %.2f ,volume %.2f\n", i+1, v[i].radius, v[i].height, volume_cone(v[i]));
     }
+    free(v);
     return 0;
 }   
